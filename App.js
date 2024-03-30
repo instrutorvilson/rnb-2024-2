0a24762fb13179b5941a3c3f9f6c2ae7d23b35aa
Aluno ProWay <aluno@proway.treina>
2024-03-30 10:36:24 -0300

    Conceitos de layout e estilização

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Dimensions, Button, Pressable } from 'react-native';
+import { TouchableOpacity } from 'react-native-web';
+const dimensionWidth = Dimensions.get('window').width
 
-export default function App() {
+export default function App() {  
+  const[msg, setMsg] = useState('Hello world')
+  //hook que gerencia estado do componente
+  //array com duas posições
+  //0 -> variavel
+  //1 -> referencia a uma função
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.topo}>
+        <Text style={{ textTransform: 'uppercase' }}>Meu primeiro componente Reactnative</Text>
+      </View>
+      <View style={[styles.topo, styles.main,{padding: 10}]}>
+        <TextInput
+          style={styles.input}
+          placeholder='Informe sua mensagem'
+          defaultValue={msg}
+          onChangeText={txt => setMsg(txt)}
+        />
+       {/* <Button
+           title='Clique aqui'
+           onPress={()=>alert('')}
+        />
+
+        <TouchableOpacity>
+          <Text>Clique aqui</Text>
+        </TouchableOpacity>
+        */}
+        <Pressable>
+          <Text>Clique aqui</Text>
+        </Pressable>
+
+        {/** lendo o valor do hook useState*/}
+        <Text>{msg}</Text>
+      </View>
+      <View style={{flex: 4, backgroundColor: 'blue', width: dimensionWidth}}>
+         <Text>Opa pessoal</Text>
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
+  topo:{
+    flex: 1, 
+    backgroundColor: 'red',
+    width: dimensionWidth
+  }, 
+  main:{
+    flex: 2, backgroundColor: 'green'
+  }, 
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'silver',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    backgroundColor: '#fff',
+    color: 'green',
+    fontWeight: '700',
+    fontSize: 18,
+    padding: 10,
+    borderBottomWidth: 2,
+    borderRadius: 10
+  }
 });
