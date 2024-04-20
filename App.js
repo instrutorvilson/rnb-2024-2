267fc864a6cd77c4f6384c027a23f8708296b9c5
Aluno ProWay <aluno@proway.treina>
2024-04-20 09:50:39 -0300

    Comunicação direta e indireta

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button, Alert } from 'react-native';
 import CardContato from './CardContato';
 
 const contatos = [
@@ -13,17 +13,45 @@ const contatos = [
   }
 ]
 
+function Filho(props){
+  return(
+     <View>
+       <Text>Titulo {props.mensagem.title}</Text>
+       <Text>Descrição {props.mensagem.descricao}</Text>
+       <Button 
+           title='Envia mensagem para o pai'
+           onPress={()=>props.onFilhoFalaProPai('ola papai')}
+       />
+     </View>
+  )
+}
+
+function Pai(){
+  const mensagem = {title:'sei lá', descricao: 'vc está deserdado'}
+
+  function mensagemRecebidaDoFilho(texto){
+    Alert.alert(texto)
+  }
+  return(
+    <View>
+         <Filho mensagem={mensagem} onFilhoFalaProPai={mensagemRecebidaDoFilho}/>         
+      </View>
+  )
+}
+
+
 export default function App() {
-  
   return (
     <View style={styles.container}>
       <Text>Lista de Contatos</Text>
       
-      <FlatList 
+      {/*<FlatList 
          data={contatos}
          renderItem={({item})=> <CardContato id={item.id} nome={item.nome}/>}
-      />      
-      <StatusBar style="auto" />
+  />      */}
+      
+      <Pai />
+
     </View>
   );
 }
