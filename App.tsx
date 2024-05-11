31691cd4c5579bf29a3a9b3362a94962c9e92e34
instrutorvilson <instrutorvilson@gmail.com>
2024-05-11 12:08:58 -0300

    Unindo navegação de diferentes tipos

--- App.tsx
+++ App.tsx
@@ -1,25 +1,25 @@
 import 'react-native-gesture-handler'
-import { StyleSheet, Text, View } from 'react-native';
-import Register from './src/adm/register';
+import { StyleSheet } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { 
+  createMaterialTopTabNavigator 
+}
+from '@react-navigation/material-top-tabs'
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
-import Login from './src/adm/login';
-import ResetarSenha from './src/adm/resetarSenha';
-import Home from './src/adm/home';
+import RouteFinanceiro from './src/rotas/routeFinaceiro';
+import RouteCompras from './src/rotas/routeCompras';
+import RouteAdm from './src/rotas/routeAdm';
 
-const Stack = createStackNavigator()
-
+const Tab = createMaterialTopTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName='home'>
-          <Stack.Screen name='register' component={Register}  />
-          <Stack.Screen name='login' component={Login} />
-          <Stack.Screen name='resetar' component={ResetarSenha} />
-          <Stack.Screen name='home' component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
+       <Tab.Navigator>
+          <Tab.Screen name='financeiro' component={RouteFinanceiro} />
+          <Tab.Screen name='compras' component={RouteCompras} />
+          <Tab.Screen name='adm' component={RouteAdm} />
+       </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
