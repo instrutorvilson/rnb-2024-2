bd929f8492c1633ada7d61c977298e49a2bfff6a
wolneihd <wolneihd@gmail.com>
2024-06-23 18:44:11 -0300

    Projeto imc criado

--- App.tsx
+++ App.tsx
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import styles from './src/estilos'
+import { useState } from 'react';
 
 export default function App() {
+  const [nome, setNome] = useState('Ana')
+  const [peso, setPeso] = useState('95.5')
+  const [altura, setAltura] = useState('1.60')
+  const [status, setStatus] = useState('')
+ 
+  function calcularIMC() {
+    let al = Number(altura)
+    let imc = Number(peso) / (al * al)
+    
+    if(imc < 18.5){
+      setStatus('Magreza')
+      return
+    }
+
+    if(imc < 24.9){
+      setStatus('Normal')
+      return
+    }
+
+    if(imc < 29.9){
+      setStatus('Sobrepeso')
+      return
+    }
+
+    if(imc < 39.9){
+      setStatus('Obesidade')
+      return
+    }
+
+    if(imc > 40.0 ){
+      setStatus('Obesidade grave')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Bem vindos ao IMC Calculator</Text>
+       <TextInput 
+          placeholder='Inform nome'
+          style={styles.input}
+          value={nome}
+          onChangeText={text => setNome(text)}
+       />
+       <TextInput 
+          placeholder='Inform altura'
+          style={styles.input}
+          value={altura}
+          onChangeText={text => setAltura(text)}
+       />
+       <TextInput 
+          placeholder='Inform peso'
+          style={styles.input}
+          value={peso}
+          onChangeText={text => setPeso(text)}
+       />
+       <Button
+         title='Carregar'
+         onPress={calcularIMC}
+       />
+
+       <View style={{marginTop: 10 }}>
+         Status: <Text style={{color: status == 'Normal' ? 'black' : 'red' }}>{status}</Text> 
+       </View>
+       
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/estilos.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native'
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    titulo:{
+      color: 'red',
+      fontSize: 28
+    },
+    input:{
+      padding: 10,
+      marginVertical: 10,
+      borderBottomWidth: 1
+    }
+  });
+
+  export default styles
+  
